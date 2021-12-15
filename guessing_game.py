9f96dd4136bd8782a4b5aa8637847deb0cf48a8b
savindu92 <savindu.edirisuriya_mohottige_de_saram@edu.devinci.fr>
2021-12-15 16:07:54 +0100

    how_many_tries

--- guessing_game.py
+++ guessing_game.py
@@ -5,6 +5,7 @@ import random
 def guess_loop():
     print("What is your name")
     name = input()
+    en_combien_essaies = 0
     number_to_guess = random.randint(1, 100)
     print("I have in mind a number in between 1 and 100, can you find it?")
     while True:
@@ -12,10 +13,12 @@ def guess_loop():
             guess = int(input())
             if guess > number_to_guess:
                 print("The number to guess is lower")
+                en_combien_essaies = en_combien_essaies + 1
             elif guess < number_to_guess:
                 print("The number to guess is higher")
+                en_combien_essaies = en_combien_essaies + 1
             else:
-                print(name, "just found the number , it was indeed",guess)
+                print(name, "just found the number with",		 en_combien_essaies,  "tries it was indeed",guess)
                 return
         except ValueError as err:
             print("Invalid input , please enter an integer")
